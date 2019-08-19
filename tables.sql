9777176dbd07a86712672bd52782da4af47ed851
Yakir Roket <yakir@superads.cn>
2019-08-19 14:39:41 +0300

    changed table fields data types to String

--- tables.sql
+++ tables.sql
@@ -31,21 +31,21 @@ CREATE TABLE `tracking_impressions`
 
 CREATE TABLE `events`
 (
-    `event_time`  DATETIME,
-    `create_id`   int(11) NOT NULL,
-    `campaign`    int(11) NOT NULL,
-    `app_id`      int(11) NOT NULL,
-    `ad_set_id`   int(11) NOT NULL,
-    `ch_id`       int(11) NOT NULL,
-    `user_id`     int(11) NOT NULL,
-    `offer_id`    int(11) NOT NULL,
-    `creative_id` int(11) NOT NULL,
-    `event_id`    int(11) NOT NULL,
-    `event_type`  int(11) NOT NULL,   //all event types
-    `geo`         varchar(5) NOT NULL,
-    `device`      varchar(20) NOT NULL,
-    `os_version`  varchar(11) NOT NULL,
-    `device_ip`   varchar(45) NOT NULL,
-    `ua`          varchar(50) NOT NULL,
+    `event_time`  DATETIME DEFAULT NULL,
+    `create_id`   varchar(11) DEFAULT NULL,
+    `campaign`    varchar(11) DEFAULT NULL,
+    `app_id`      varchar(11) DEFAULT NULL,
+    `ad_set_id`   varchar(11) DEFAULT NULL,
+    `ch_id`       varchar(11) DEFAULT NULL,
+    `user_id`     varchar(11) DEFAULT NULL,
+    `offer_id`    varchar(11) DEFAULT NULL,
+    `creative_id` varchar(11) DEFAULT NULL,
+    `event_id`    varchar(11) DEFAULT NULL,
+    `event_type`  varchar(11) DEFAULT NULL,
+    `geo`         varchar(5) DEFAULT NULL,
+    `device`      varchar(20) DEFAULT NULL,
+    `os_version`  varchar(11) DEFAULT NULL,
+    `device_ip`   varchar(45) DEFAULT NULL,
+    `ua`          text CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
     KEY (`event_id`) USING CLUSTERED COLUMNSTORE
 );
